6ec3f34aab2b8424bc008d6a155991b9016c7735
Suresh <sureshkumar.v@thoughtworks.com>
2021-12-07 11:15:47 +0530

    Suresh/Kanishk | Test the CI/CD negative test case

--- src/test/java/com/tw/bootcamp/bookshop/BookshopApplicationTests.java
+++ src/test/java/com/tw/bootcamp/bookshop/BookshopApplicationTests.java
@@ -1,5 +1,6 @@
 package com.tw.bootcamp.bookshop;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class BookshopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Fail the test case");
 	}
 
 }
