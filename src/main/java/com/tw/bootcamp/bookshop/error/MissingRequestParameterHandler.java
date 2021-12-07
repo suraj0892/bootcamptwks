6e998259963685cc7a1ae69731311961eb227d6f
Ankit Jaiswal <ankit@Ankits-MacBook-Pro.local>
2021-12-07 15:52:18 +0530

    Ankit/Saugata | Fix. Missing request parameter handler

--- src/main/java/com/tw/bootcamp/bookshop/error/MissingRequestParameterHandler.java
+++ src/main/java/com/tw/bootcamp/bookshop/error/MissingRequestParameterHandler.java
@@ -3,8 +3,10 @@ package com.tw.bootcamp.bookshop.error;
 import com.tw.bootcamp.bookshop.book.InvalidRequestParameterException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class MissingRequestParameterHandler {
     @ExceptionHandler({InvalidRequestParameterException.class})
     public ResponseEntity<ErrorResponse> handleMissingRequestParameterError(InvalidRequestParameterException ex) {
