a3e5cc0145768a812ffac4dace96e471e05426cc
suraj.krishnan <suraj.krishnan@thoughtworks.com>
2021-12-09 13:31:12 +0530

    Suraj/Saugata | Refactor. Add. PaymentGateway Service

--- src/test/java/com/tw/bootcamp/bookshop/payment/PaymentGatewayServiceTest.java
+++ src/test/java/com/tw/bootcamp/bookshop/payment/PaymentGatewayServiceTest.java
@@ -2,10 +2,8 @@ package com.tw.bootcamp.bookshop.payment;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
